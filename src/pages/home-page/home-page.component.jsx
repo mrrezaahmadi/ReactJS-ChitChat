75e5ec565df12e91e9cfa63b1ad38a93a42aa08d
mrrezaahmadi <mrrezaahmadi>
2020-09-28 19:39:14 +0330

    set up a template of what is supposed to be in home page

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-function Home() {
-	return <div>This is Home page</div>;
-}
+// Styles
+import { Grid } from "semantic-ui-react";
+import "./home-page.styles.scss";
+
+// Components
+import ColorPanel from "../../components/color-panel/color-panel.component";
+import SidePanel from "../../components/side-panel/side-panel.component";
+import MetaPanel from "../../components/meta-panel/meta-panel.component";
+import Messages from "../../components/messages/messages.component";
+
+const Home = () => (
+	<Grid columns="equal" className="Home" style={{ background: "#eee" }}>
+		<ColorPanel />
+		<SidePanel />
+
+		<Grid.Column style={{ marginLeft: 320 }}>
+			<Messages />
+		</Grid.Column>
+
+		<Grid.Column width={4}>
+			<MetaPanel />
+		</Grid.Column>
+	</Grid>
+);
 
 export default Home;
