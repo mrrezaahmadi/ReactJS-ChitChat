2dbb6302a2ccd1e37f5fd42555570ffc8799ca35
mrrezaahmadi <muhammadrezaahmadiii@gmail.com>
2020-09-29 19:12:16 +0330

    fix some minor issues

--- src/components/file-modal/file-modal.component.jsx
+++ src/components/file-modal/file-modal.component.jsx
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 import mime from "mime-types";
 import { Modal, Input, Button, Icon } from "semantic-ui-react";
 
-const FileModal = (props) => {
-	const { modal, closeModal, uploadFile } = props;
-	const [file, setFile] = useState(null);
-	const authorizedTypes = ["image/jpeg", "image/png"];
+const FileModal = ({ modal, closeModal, uploadFile }) => {
+	const [state, setState] = useState({
+		file: null,
+		authorized: ["image/jpeg", "image/png"]
+	})
+	const { file, authorized } = state
 
 	const addFile = (e) => {
 		const file = e.target.files[0];
 		console.log(file);
 		if (file) {
-			setFile(file);
+			setState({ ...state, file })
 		}
 	};
 
@@ -21,17 +23,17 @@ const FileModal = (props) => {
 				const metaData = { contentType: mime.lookup(file.name) };
 				uploadFile(file, metaData);
 				closeModal();
-				setFile(null);
+				clearFile()
 			}
 		}
 	};
 
 	const isAuthorized = (fileType) => {
-		return authorizedTypes.includes(mime.lookup(fileType));
+		return authorized.includes(mime.lookup(fileType));
 	};
 
 	const clearFile = () => {
-		setFile(null);
+		setState({ ...state, file: null })
 	};
 
 	return (
