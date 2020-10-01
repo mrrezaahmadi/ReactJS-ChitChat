b000958e788d74923ed741763df443f594e02a58
mrrezaahmadi <mrrezaahmadi>
2020-10-01 14:45:25 +0330

    scroll down automaticaly  the messages component when a new message added

--- src/components/messages/messages.component.jsx
+++ src/components/messages/messages.component.jsx
@@ -42,6 +42,16 @@ class Messages extends React.Component {
 		}
 	}
 
+	componentDidUpdate(prevProps, PrevState) {
+		if (this.messagesEnd) {
+			this.scrollToBottom();
+		}
+	}
+
+	scrollToBottom = () => {
+		this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+	};
+
 	addListeners = (channelId) => {
 		this.addMessageListener(channelId);
 		this.addTypingListener(channelId);
@@ -260,6 +270,7 @@ class Messages extends React.Component {
 							? this.displayMessages(searchResults)
 							: this.displayMessages(messages)}
 						{this.displayTypingUsers(typingUsers)}
+						<div ref={(node) => (this.messagesEnd = node)}></div>
 					</Comment.Group>
 				</Segment>
 
